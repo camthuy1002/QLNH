203eb6c1eb066bb81f436ae7cd363afa33aa609c
Cẩm Thúy Lê <lecamthuy1002@gmail.com>
2022-10-30 13:46:03 +0700

    Add project files.

---
+++ QLNH.WebApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace QLNH.WebApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
